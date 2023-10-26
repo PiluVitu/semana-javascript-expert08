93cd4825fb02084f6791124ce1e5385b69322b63
Paulo Victor Torres Silva <pilutechinformatica@gmail.com>
2023-10-26 13:56:45 -0300

    feat: add files of first class

---
+++ aula01-mp4demuxer-canvas-render/app/pages/file-upload/src/app.js
@@ -0,0 +1,50 @@
+import Clock from './deps/clock.js'
+
+import View from './view.js'
+
+const view = new View()
+const clock = new Clock()
+
+const worker = new Worker('./src/worker/worker.js', { type: 'module' })
+
+worker.onerror = error => console.error('Error no worker', error)
+worker.onmessage = ({ data }) => {
+  if (data.status !== 'done') return
+  clock.stop()
+  view.updateElapsedTime(`Process took ${took.replace('ago', '')}`)
+}
+
+let took = ''
+
+view.configureOnFileChange(file => {
+  worker.postMessage({
+    file
+  })
+
+  clock.start(time => {
+    took = time
+    view.updateElapsedTime(`Process started ${time}`)
+  })
+})
+
+async function fakeFetch() {
+  const filePath = '/videos/frag_bunny.mp4'
+  const response = await fetch(filePath)
+
+  // Traz o tamanho do arquivo
+  // const response = await fetch(filePath, { method: 'HEAD' })
+  // console.log(response.headers.get('Content-Length'))
+  // debugger
+
+  const file = new File([await response.blob()], filePath, {
+    type: 'video/mp4',
+    lastModified: Date.now()
+  })
+  const event = new Event('change')
+  Reflect.defineProperty(event, 'target', {
+    value: { files: [file] }
+  })
+  document.getElementById('fileUpload').dispatchEvent(event)
+}
+
+fakeFetch()
---
+++ aula01-mp4demuxer-canvas-render/app/pages/file-upload/src/worker/worker.js
@@ -0,0 +1,46 @@
+import VideoProcessor from './videoProcessor.js'
+import MP4Demuxer from './mp4Demuxer.js'
+
+const qvgaConstrains = {
+  width: 320,
+  height: 240
+}
+const vgaConstrains = {
+  width: 640,
+  height: 480
+}
+const hdConstrains = {
+  width: 1280,
+  height: 720
+}
+
+const encoderConfig = {
+  ...qvgaConstrains,
+  ...vgaConstrains,
+  ...hdConstrains,
+  bitrate: 10e6,
+  //WebM
+  codec: 'vp09.00.10.08',
+  pt: 4,
+  hardwareAcelleration: 'prefer-software'
+  // //MP4
+  // codec: 'avc1.42002A',
+  // pt: 1,
+  // hardwareAcelleration: 'prefer-hardware',
+  // avc: { format: 'annexb' }
+}
+
+const mp4Demuxer = new MP4Demuxer()
+const videoProcessor = new VideoProcessor({ mp4Demuxer })
+
+onmessage = async ({ data }) => {
+  await videoProcessor.start({
+    file: data.file,
+    encoderConfig,
+    sendMessage(message) {
+      self.postMessage(message)
+    }
+  })
+
+  self.postMessage({ status: 'done' })
+}
